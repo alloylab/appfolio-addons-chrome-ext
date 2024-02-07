5b474089ed8a04488208ce0e75285d4e7c92e587
Trevor Ferre <trevor@alloylab.com>
2024-02-07 13:23:12 -0600

    changes notes script to jquery

--- src/notes/script.js
+++ src/notes/script.js
@@ -1,18 +1,10 @@
 setTimeout(function () {
-    let decodeHTML = function (html) {
-        let txt = document.createElement('textarea');
-        txt.innerHTML = html;
-        return txt.value;
-    };
-
-    let note_contents = document.getElementsByClassName('note__contents');
-
-    for (let i = 0, l = note_contents.length; i < l; i++) {
-        let contentElement = note_contents[i].getElementsByClassName('js-note-text')[0];
-
-        let content = decodeHTML(contentElement.innerHTML.replace(/<\bspan\b>/g, '').replace(/<\/\bspan\b>/g, '').trim());
+    $('.note__contents').each(function () {
+        let note = $(this).find('.js-note-text');
+        let content = note.html().replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/<\bspan\b>/g, '').replace(/<\/\bspan\b>/g, '').trim();
 
         let converter = new showdown.Converter();
-        contentElement.innerHTML = converter.makeHtml(content);
-    }
+        let markdown = converter.makeHtml(content);
+        $(this).replaceWith(markdown);
+    });
 }, 1000);
