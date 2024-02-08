48dcc31ec9f28ebd8a0db1e0fab0072100433a98
Trevor Ferre <trevor@alloylab.com>
2024-02-08 16:17:48 -0600

    showdown to marked
    change from shown to marked due library security issues

--- src/notes/script.js
+++ src/notes/script.js
@@ -7,8 +7,7 @@ const notesObserver = new MutationObserver(function () {
             let content = note.html().replace(/&lt;/g, '<').replace(/&gt;/g, '>')
                 .replace(/<\bspan\b>/g, '').replace(/<\/\bspan\b>/g, '').trim();
 
-            let converter = new showdown.Converter();
-            let markdown = converter.makeHtml(content);
+            let markdown = DOMPurify.sanitize(marked.parse(content));
             $(this).replaceWith(markdown);
         });
     }
